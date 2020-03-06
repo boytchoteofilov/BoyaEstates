f84a2f47288d8aac0d1c2f8ae7160acf7e56c380
Boytcho Teofilov <boytchoteofilov@users.noreply.github.com>
2020-03-06 19:57:55 +0000

    add Application context to the startup services

---
+++ BoyaEstates.Data/BoyaEstatesDbContext.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace BoyaEstates.Data
+{
+    public class BoyaEstatesDbContext : DbContext
+    {
+        public BoyaEstatesDbContext(DbContextOptions options) : base(options)
+        {
+        }
+
+        public DbSet<Property> Properties { get; set; }
+    }
+}
