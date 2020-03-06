2dfc20290c9d4cfe2a585219cdc21f8a2b3bfff9
Boytcho Teofilov <boytchoteofilov@users.noreply.github.com>
2020-03-06 19:34:36 +0000

    Added Data Layer, BaseModel and MainModel

---
+++ BoyaEstates.Data/Property.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace BoyaEstates.Data
+{
+    public class Property : BaseModel<int>
+    {
+        public int Price { get; set; }
+
+        public string Description { get; set; }
+
+        public int Bedrooms { get; set; }
+
+        public int Bathrooms { get; set; }
+
+        public int Area { get; set; }
+
+        public int Floor { get; set; }
+    }
+}
